7b3f37830654067bf843e7473d635b3c36423ce3
ttabor16 <ttabor16@gmail.com>
2025-11-13 20:23:20 -1000

    verbose argument, refactored positional argument, implement initial calculator

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 import os
 import sys
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    args = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(exit_on_error=False)
+    parser.add_argument("prompt", nargs="?")
+    parser.add_argument("-v", "--verbose", action="store_true", help = "Enable verbose output")
+    args = parser.parse_args()
     
-    if not args:
+    if not args.prompt:
         print('No prompt was provided.')
         print('\nUsage: python main.py "your prompt here:"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    prompt = " ".join(args)
+    user_prompt = ' '.join(args.prompt)
 
+    if args.verbose:
+        print(f'User prompt: {user_prompt}')
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)],)
+    ]
+
+    generate_content(client, messages, args)
+
+def generate_content(client, messages, args):
     response = client.models.generate_content(
         model = 'gemini-2.0-flash-001',
-        contents = prompt
+        contents = messages
     )
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-    print(f'Response: {response.text}')
-    print(f'Prompt tokens: {prompt_tokens}')
-    print(f'Response tokens: {response_tokens}')
 
+    if args.verbose:
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    print(f'Response: {response.text}')
 
 if __name__ == "__main__":
     main()
